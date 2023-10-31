be0841827001ed7895d55027c15a87f2eee3444b
Marcus Martins <89209017+marcusmartinss@users.noreply.github.com>
2023-10-31 00:07:50 +0000

    Esqueleto do projeto

---
+++ script.js
@@ -0,0 +1,77 @@
+let tasks = [];
+
+function addTask() {
+    const taskName = document.getElementById('task').value;
+    const dueDate = document.getElementById('due-date').value;
+    const dueTime = document.getElementById('due-time').value;
+    const taskDuration = parseFloat(document.getElementById('duration').value);
+
+    if (taskName !== '' && dueDate !== '' && dueTime !== '' && !isNaN(taskDuration) && taskDuration > 0) {
+        const taskDate = new Date(`${dueDate} ${dueTime}`);
+        tasks.push({ name: taskName, date: taskDate, duration: taskDuration });
+        document.getElementById('task-ul').innerHTML += `<li>${taskName} - ${taskDate.toLocaleString()} - ${taskDuration} hours</li>`;
+        document.getElementById('task').value = '';
+        document.getElementById('due-date').value = '';
+        document.getElementById('due-time').value = '';
+        document.getElementById('duration').value = '';
+    }
+}
+
+function quicksort(tasks, left, right) {
+    if (left < right) {
+        const pivotIndex = partition(tasks, left, right);
+        quicksort(tasks, left, pivotIndex - 1);
+        quicksort(tasks, pivotIndex + 1, right);
+    }
+}
+
+function partition(tasks, left, right) {
+    const pivot = tasks[right].date;
+    let i = left - 1;
+
+    for (let j = left; j < right; j++) {
+        if (tasks[j].date <= pivot) {
+            i++;
+            swap(tasks, i, j);
+        }
+    }
+
+    swap(tasks, i + 1, right);
+    return i + 1;
+}
+
+function swap(tasks, i, j) {
+    const temp = tasks[i];
+    tasks[i] = tasks[j];
+    tasks[j] = temp;
+}
+
+function prioritizeTasks() {
+    quicksort(tasks, 0, tasks.length - 1);
+
+    const resultUl = document.getElementById('result-ul');
+    resultUl.innerHTML = '';
+
+    const currentDate = new Date();
+
+    tasks.forEach((task) => {
+        const isLate = task.date < currentDate;
+        const formattedDate = task.date.toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' });
+        let taskText = `${task.name} - ${formattedDate} - Estimated Duration: ${task.duration} hours`;
+
+        if (isLate) {
+            taskText = `[ATRASADO] ${taskText}`;
+        }
+
+        const taskColor = isLate ? 'red' : 'black';
+
+        resultUl.innerHTML += `<li style="color: ${taskColor};">${taskText}</li>`;
+
+        const startTime = currentDate < task.date ? task.date : currentDate;
+        const endTime = new Date(startTime.getTime() + task.duration * 60 * 60 * 1000);
+        resultUl.innerHTML += `<li>Scheduled from ${startTime.toLocaleTimeString()} to ${endTime.toLocaleTimeString()}</li>`;
+
+        currentDate.setTime(endTime.getTime());
+    });
+}
+
